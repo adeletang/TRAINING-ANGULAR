5da1914e15a1fe9826dfe189f7408fd86fde8188
adeletang <72740943+adeletang@users.noreply.github.com>
2021-02-15 15:37:45 +0100

    lazy loading

--- lp-fi/src/app/app-routing.module.ts
+++ lp-fi/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { NotFoundComponent } from './shared/components/not-found/not-found.compo
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
   },
   {
     path: '**',
