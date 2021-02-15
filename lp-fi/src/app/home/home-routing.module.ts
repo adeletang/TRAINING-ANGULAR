87561449e76e08f682bf8a02101508c121f0d7e2
adeletang <72740943+adeletang@users.noreply.github.com>
2021-02-15 15:45:32 +0100

    add routes to home-routing.module

--- lp-fi/src/app/home/home-routing.module.ts
+++ lp-fi/src/app/home/home-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from '../shared/components/not-found/not-found.component';
+import { HomeComponent } from './home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
