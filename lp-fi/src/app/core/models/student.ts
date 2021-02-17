4c1dd31a354a60b327da77753850744fdfef688b
adeletang <72740943+adeletang@users.noreply.github.com>
2021-02-17 16:57:11 +0100

    add models/students.ts
    interface de student

---
+++ lp-fi/src/app/core/models/student.ts
@@ -0,0 +1,9 @@
+import { stringify } from "@angular/compiler/src/util"
+
+export interface Student{
+    id: number;
+    firstName: string;
+    lastName: string;
+    brithYear: number;
+    class: string;
+}
