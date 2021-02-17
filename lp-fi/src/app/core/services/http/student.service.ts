b46948fb5e7fc3a5b3f813852dba6dfd9a59aa98
adeletang <72740943+adeletang@users.noreply.github.com>
2021-02-17 17:22:36 +0100

    student

---
+++ lp-fi/src/app/core/services/http/student.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Student } from '../../models/student';
+
+@Injectable()
+export class StudentService {
+
+  constructor(private _httpClient: HttpClient) { }
+
+  get(): Observable<Student> {
+    return this._httpClient.get<Student>("http://localhost:3000/students");
+  }
+  
+}
